f9961a65225c4f30915fe8ab795c1c1ad9b6157e
kxw <ken.wonoatmojo@spatialvision.com.au>
2015-04-30 10:59:07 +1000

    refactor config to under examples

--- examples/config.js
+++ examples/config.js
@@ -1,10 +1,14 @@
 'use strict';
 
-if (!window.farmbuild) {
-	window.farmbuild = {};
-	window.farmbuild.wfsSoilArea = {};
+if (!window.farmbuild.webservices) {
+	window.farmbuild.webservices = {};
 }
 
-window.farmbuild.wfsSoilArea.demo = {
-		version: '0.1'
+window.farmbuild.webservices.examples = {
+		version: '0.2',
+		authentication : {
+			auth : 'https://farmbuild-sts.dev.ag.ecodev.vic.gov.au/core/connect/token',
+			clientId : 'TESTCLIENT',
+			clientSecret : 'testClientSecret'
+		}
 };
---
+++ examples/confige2e.js
@@ -0,0 +1,28 @@
+
+module.exports = (function() {
+  'use strict';
+
+  //skip self signed cer check
+  //https://github.com/visionmedia/superagent/issues/188
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+  var confige2e = {};
+
+  confige2e.authentication = {
+      scopes : {
+        WFS : 'WFS_SERVICES'
+      },
+      url : 'https://farmbuild-sts.dev.ag.ecodev.vic.gov.au/core/connect/token',
+      scope : 'WFS_SERVICES',
+      clientId : 'TESTCLIENT',
+      clientSecret : 'testClientSecret'
+      };
+
+  confige2e.wfs = {
+      soils : 'https://farmbuild-ws.dev.ag.ecodev.vic.gov.au/soils'
+      };
+
+  console.log('config created: %j', confige2e);
+
+  return confige2e;
+});
