9d1f32ea4d70eadfbdb68143faacf9dd4cbfa887
kxw <ken.wonoatmojo@spatialvision.com.au>
2015-05-05 09:42:06 +1000

    finalise example

--- examples/angularjs/soilareas/index.js
+++ examples/angularjs/soilareas/index.js
@@ -19,24 +19,20 @@ angular.module('farmbuild.webservices.examples.soilarea', [])
 			$scope.mode = null;
 		}
 
-		$scope.connectSoilArea = function(wfsUrl, token, proxyUrl, farmdata) {
-/*			var res = $http({
-				method: 'GET',
-				data : ''
-			})*/
+		$scope.connectWithToken = function(wfsUrl, token, farmdata) {
+			$scope.error = false;
+			$scope.errorMessages = [];
+			$scope.messages = [];
 			var reqConfig = {
 				method: 'POST',
 				data : farmdata
 			};
-			if ('PROXY' === $scope.mode) {
-				reqConfig.url = proxyUrl;
-			}
-			else  {
-				reqConfig.url = wfsUrl;
-				reqConfig.headers= {
-					'Authorization': 'Bearer ' + token
-				};
-			}
+
+			reqConfig.url = wfsUrl;
+			reqConfig.headers= {
+				'Authorization': 'Bearer ' + token
+			};
+
 			var res = $http(reqConfig);
 			res.success(function(data, status, headers, config) {
 				$scope.messages.push("Successfully connect to WFS service.  Result:");
@@ -48,12 +44,24 @@ angular.module('farmbuild.webservices.examples.soilarea', [])
 			});
 		}
 
-		$scope.connectWithToken = function() {
-			$scope.mode = 'TOKEN';
-		};
-
-		$scope.connectViaProxy = function() {
-			$scope.mode = 'PROXY';
+		$scope.connectViaProxy = function(proxyUrl, farmdata) {
+			$scope.error = false;
+			$scope.errorMessages = [];
+			$scope.messages = [];
+			var reqConfig = {
+				method: 'POST',
+				data : farmdata,
+				url : proxyUrl
+			};
+			var res = $http(reqConfig);
+			res.success(function(data, status, headers, config) {
+				$scope.messages.push("Successfully connect to WFS service.  Result:");
+				$scope.messages.push(data);
+			});
+			res.error(function(data, status, headers, config) {
+				$scope.error = true;
+				$scope.errorMessages.push("Error connecting to WFS "+status);
+			});
 		}
 
 		$scope.reset();
