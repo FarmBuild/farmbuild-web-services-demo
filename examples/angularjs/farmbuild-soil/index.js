451dc6ef485e3a9450d6f9b430773d75d24f6eaf
Parham Fazel <parham-fazel@users.noreply.github.com>
2015-11-02 10:00:43 +1100

    Reviewed docs

--- examples/angularjs/farmbuild-soil/index.js
+++ examples/angularjs/farmbuild-soil/index.js
@@ -1,11 +1,30 @@
 'use strict';
 
 /**
- * This example page is developed using the JavaScript MVC framework called AngularJS.
+ * AngularJS is popular JavaScript MVC framework which is developed by google.
+ * In this example we use AngularJS to construct the structure of the client side application.
  * You can find out more about AngularJS at https://angularjs.org
+ * In farmbuild project we have used AngularJS as an internal dependency to provide modular structure, but to use FarmBuild JavaScript libraries you are forced to use AngularJS.
+ * All the api function are available via "farmbuild" namespace (eg: farmbuild.webmapping, farmbuild.nutrientcalculator).
+ * If you are using AngularJS in your application you can consume farmbuild component as AngularJS modules, similar to this example.
+ */
+ 
+ /**
+ * Defining my application. There are no dependency at this point so I am passing an empty array.
  */
 angular.module('farmbuild.webservices.examples.soil', ['farmbuild.farmdata', 'farmbuild.webservices.examples.soil'])
-
+	/**
+	 * In AngularJS Every application has a single root scope.
+	 * All other scopes are descendant scopes of the root scope.
+	 * Scopes provide separation between the model and the view, via a mechanism for watching the model for changes.
+	 * They also provide an event emission/broadcast and subscription facility.
+	 * See the AngularJS developer guide on scopes.
+	 * https://docs.angularjs.org/guide/scope
+	 */
+	 
+ 	/**
+	 * "run" method is executed before any other function in application, so I am putting my initial configs here.
+	 */
     .run(function ($rootScope) {
         /**
          * Read the application version from the config.js file located in examples directory
@@ -13,7 +32,9 @@ angular.module('farmbuild.webservices.examples.soil', ['farmbuild.farmdata', 'fa
         $rootScope.appVersion = farmbuild.webservices.examples.version;
 
     })
-
+/**
+	 * "controller" is where I put the logic of my application
+	 */
     .controller('SoilArea', function ($scope, $http, $log, farmdata, soilarea) {
 
         /**
@@ -32,7 +53,8 @@ angular.module('farmbuild.webservices.examples.soil', ['farmbuild.farmdata', 'fa
 
         /**
          * Executes Ajax POST with the given request configuration.
-         *
+         * The $http service is a core Angular service that facilitates communication
+         * with the remote HTTP servers via the browser's XMLHttpRequest object or via JSONP.
          * @param reqConfig     Ajax request configuration
          */
         function execute(reqConfig) {
